b1547471e2bf1bc1b039d6ac4c32b6f993e0bd69
Andrew Hooker <andrew.c.hooker@gmail.com>
2018-05-15 14:54:21 +0200

    tests for force_extrapolate

--- tests/testthat/test-ncappc.R
+++ tests/testthat/test-ncappc.R
@@ -454,3 +454,64 @@ test_that("IV infusion multiple dose data is computed corectly",{
   
 })
 
+test_that("force_extrapolate works corectly",{
+
+  data_1 <- tibble::tibble(
+    ID=1,
+    TIME = c(0,0.25,0.5,1,1.5,2,3,4,6,8,12,16,24),
+    DV=c(0, 0.07, 0.14, 0.21, 0.24, 0.27, 0.26, 0.25, 0.22, 0.19, 0.13, 0.081, 0.033)
+  )
+
+  out_1 <- ncappc(obsFile=data_1,
+                  onlyNCA = T,
+                  extrapolate = T,
+                  printOut = F,
+                  evid = FALSE,
+                  noPlot = T)
+  
+  out_2 <- ncappc(obsFile=data_1,
+                  onlyNCA = T,
+                  extrapolate = T,
+                  printOut = F,
+                  evid = FALSE,
+                  noPlot = T,
+                  force_extrapolate=T)
+  
+  expect_true(all(out_1$ncaOutput==out_2$ncaOutput,na.rm = T))
+  
+  data_2 <- data_1 %>% dplyr::filter(TIME<3) 
+  
+  expect_message(out_2 <- ncappc(obsFile=data_2,
+                  onlyNCA = T,
+                  extrapolate = T,
+                  printOut = F,
+                  evid = FALSE,
+                  noPlot = T,
+                  force_extrapolate=T))
+  expect_true(is.na(out_2$ncaOutput$AUCINF_obs))
+    
+  data_3 <- data_1 %>% dplyr::filter(TIME>17|TIME<3) 
+  
+  out_3 <- ncappc(obsFile=data_3,
+                  onlyNCA = T,
+                  extrapolate = T,
+                  printOut = F,
+                  evid = FALSE,
+                  noPlot = T,
+                  force_extrapolate=T)
+  expect_true(!is.na(out_3$ncaOutput$AUCINF_obs))
+  
+  data_4 <- data_1 %>% dplyr::filter(TIME>15|TIME<3) 
+  
+  out_4 <- ncappc(obsFile=data_4,
+                  onlyNCA = T,
+                  extrapolate = T,
+                  printOut = F,
+                  evid = FALSE,
+                  noPlot = T,
+                  force_extrapolate=T)
+  expect_true(!is.na(out_4$ncaOutput$AUCINF_obs))
+  
+  
+})
+  
